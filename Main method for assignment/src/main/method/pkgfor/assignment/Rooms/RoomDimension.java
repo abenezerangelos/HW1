a5fec285049f326ed1557022589037a7daa60900
Ebenezer <abenezerangel@gmail.com>
2020-09-05 02:17:55 -0700

    Create RoomDimension.java

---
+++ Main method for assignment/src/main/method/pkgfor/assignment/Rooms/RoomDimension.java
@@ -0,0 +1,46 @@
+package main.method.pkgfor.assignment;
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abenezerfetsum
+ */
+public class RoomDimension {
+
+   static double  length;
+     static double  width;
+     public static void main(String[] args) {
+        
+      
+       
+     
+    }
+    RoomDimension(double l,double w){
+       length=l;
+       width=w;
+       
+    }
+    static double getArea(double length, double width){
+        double Area= length*width;
+        return Area;
+    }
+    
+
+    
+    
+    public double  toString(double Area){
+        
+        return Area;
+    }  
+    
+    
+}
+
+    
